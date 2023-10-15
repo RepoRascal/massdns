59c8ac86deb7e44ca708f3ffc4bc31f9c277d7ce
RepoRascal <126687635+RepoRascal@users.noreply.github.com>
2023-10-15 08:52:14 +0530

    Update ptr.py

--- scripts/ptr.py
+++ scripts/ptr.py
@@ -1,9 +1,13 @@
-#!/usr/bin/python
-
 # Print all possible IPv4 addresses
-# May be useful for resolving PTR records via MassDNS /dev/stdin
+# Modified to start from a specific IP address for resolving PTR records via MassDNS /dev/stdin
 
 from itertools import product
 
-for a, b, c, d in product(range(256), repeat=4):
-	print("%d.%d.%d.%d.in-addr.arpa" % (a, b, c, d))
+# Define starting values
+start_a, start_b, start_c, start_d = 150, 100, 41, 203
+
+for a, b, c, d in product(range(start_a, 256), 
+                          range(start_b if start_a == 145 else 0, 256), 
+                          range(start_c if start_b == 207 else 0, 256), 
+                          range(start_d if start_c == 47 else 0, 256)):
+    print("%d.%d.%d.%d.in-addr.arpa" % (a, b, c, d))
